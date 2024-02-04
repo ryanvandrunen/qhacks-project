8541a7bb4a44020b8ce24269dc1425dcbfdb06ee
madisonmacneil <maccmadison@gmail.com>
2024-02-04 00:36:18 -0500

    location and postal

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,24 +1,16 @@
-import React, { useState } from 'react';
+
 import { db } from '../firebase';
-import { collection, addDoc } from 'firebase/firestore';
-import { Button, FormControl } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { FormControl, Button } from 'react-bootstrap';
+import { fetchDataFromWebpage } from './postal';
 
 export default function GetPostalCode() {
   const [postalCode, setPostalCode] = useState('');
 
-  const addPostalCode = async () => {
-    try {
-      const docRef = await addDoc(collection(db, 'postalCodes'), {
-        code: postalCode
-      });
-      console.log('Postal code added with ID: ', docRef.id);
+  const handlePostalCodeSubmit = async () => {
+    const data = fetchDataFromWebpage(postalCode, 'Walmart')
+  }
 
-      // Clear the input field after successful submission
-      setPostalCode('');
-    } catch (error) {
-      console.error('Error adding postal code: ', error);
-    }
-  };
 
   return (
     <div>
@@ -32,7 +24,7 @@ export default function GetPostalCode() {
         <Button
           type="button"
           className="btn-warning"
-          onClick={addPostalCode}
+          onClick={handlePostalCodeSubmit}
         >
           Submit
         </Button>
